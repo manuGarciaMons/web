fe6b7cd4cbd9e92567b4efc3005c7debae1ebb45
Manuela García M <42476682+manuGarciaMons@users.noreply.github.com>
2020-02-18 19:29:36 -0500

    Add files via upload

---
+++ clase2/inicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Formulario</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <form action="validar.php" method="POST">
+        <table>
+        <tr>
+        <td> AÑO NACIMIENTO:  </td>
+        <td><input type="text" name="anio" id="anio"></td>
+        </tr>
+        </table>
+    <div>   
+    <input type="submit" value="VALIDAR">
+    </div>
+    </form>
+</body>
+</html>
---
+++ clase2/validar.php
@@ -0,0 +1,8 @@
+
+
+<?php
+$ano_nac =$_POST["anio"];
+$edad = (2020 - $ano_nac);
+echo "El año de  nacimiento es: " .$ano_nac. "<br>";
+echo "la edad es: " .$edad ;
+?>
